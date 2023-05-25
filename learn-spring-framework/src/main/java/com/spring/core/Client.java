c1ba4e596a28b5794d0a8f806b43fb8099243f8c
Fernando Salas <luisfernandosalasg@gmail.com>
2023-05-25 17:05:29 -0500

    IOC Container

--- learn-spring-framework/src/main/java/com/spring/core/Client.java
+++ learn-spring-framework/src/main/java/com/spring/core/Client.java
@@ -1,10 +1,26 @@
 package com.spring.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Client {
     public static void main(String[] args) {
-        Vehicle vehicle = new Cycle();
-        Traveler traveler = new Traveler(vehicle);
-        traveler.startJourney();
+//        Vehicle vehicle = new Car();
+//        Traveler traveler = new Traveler(vehicle);
+//        traveler.startJourney();
 
+        // Creating Spring IOC Container
+        // Read the configuration class
+        // Create and manage the spring beans
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // Retrieve Spring Beans from IOC Container
+        Car car = applicationContext.getBean(Car.class);
+        car.move();
+
+        Bike bike = applicationContext.getBean(Bike.class);
+        bike.move();
+
+        Traveler traveler1 = applicationContext.getBean(Traveler.class);
+        traveler1.startJourney();
     }
 }
