81be86e00873d807601f822c398e4dcb5282827e
Fernando Salas <luisfernandosalasg@gmail.com>
2023-05-26 07:36:28 -0500

    No Dependency Injection

---
+++ learn-spring-framework/src/main/java/com/spring/core/di/Client.java
@@ -0,0 +1,11 @@
+package com.spring.core.di;
+
+public class Client {
+    public static void main(String[] args) {
+        String message = "Hi good morning have a nice day";
+        SMSService smsService = new SMSService();
+        EmailService emailService = new EmailService();
+        MessageSender messageSender = new MessageSender(smsService);
+        messageSender.sendMessage(message);
+    }
+}
